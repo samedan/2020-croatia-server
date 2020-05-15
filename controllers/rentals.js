706c911feeca868bf4a92a2199c56058519057db
samedan <same.dan@gmail.com>
2020-05-15 13:56:44 +0200

    Finished Booking

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -1,36 +1,40 @@
-
 const Rental = require('../models/rental');
 
 exports.getRentals = (req, res) => {
   Rental.find({}, (error, foundRentals) => {
-    if (error) { return res.mongoError(error); }
+    if (error) {
+      return res.mongoError(error);
+    }
 
     return res.json(foundRentals);
-  })
-}
+  });
+};
 
-exports.getRentalById = (req, res) => {
+exports.getRentalById = async (req, res) => {
   const { rentalId } = req.params;
-  
-  Rental.findById(rentalId, (error, foundRental) => {
-    if (error) { return res.mongoError(error); }
-
-    return res.json(foundRental)
-  })
-}
+  try {
+    const rental = await Rental.findById(rentalId).populate(
+      'owner',
+      '-password -_id'
+    );
+    return res.json(rental);
+  } catch (error) {
+    return res.mongoError(error);
+  }
+};
 
 exports.createRental = (req, res) => {
   const rentalData = req.body;
   rentalData.owner = res.locals.user;
 
   Rental.create(rentalData, (error, createdRental) => {
-    if (error) { return res.mongoError(error); }
+    if (error) {
+      return res.mongoError(error);
+    }
 
     return res.json(createdRental);
-  })
-}
-
-
+  });
+};
 
 // middlewares
 
@@ -38,19 +42,26 @@ exports.isUserRentalOwner = (req, res, next) => {
   const { rental } = req.body;
   const user = res.locals.user;
 
-  Rental
-    .findById(rental)
+  if (!rental) {
+    return res.sendApiError({
+      title: 'Booking Error',
+      detail: 'Cannot create booking on undefined rental',
+    });
+  }
+  Rental.findById(rental)
     .populate('owner')
     .exec((error, foundRental) => {
-      if (error) { return res.mongoError(error); }
+      if (error) {
+        return res.mongoError(error);
+      }
 
       if (foundRental.owner.id === user.id) {
-        return res
-          .sendApiError(
-            { title: 'Invalid User', 
-              detail: 'Cannot create booking on your rental'});
+        return res.sendApiError({
+          title: 'Invalid User',
+          detail: 'Cannot create booking on your rental',
+        });
       }
 
       next();
-    })
-}
+    });
+};
